e00cde92ce4291e7647b5540dd046520b3973582
wz2793312458 <wz2793312458@163.com>
2017-07-31 17:05:37 +0800

    Initial commit

---
+++ app/src/main/java/com/hail/autoscrollbacklayoutdemo/MainActivity.java
@@ -0,0 +1,32 @@
+package com.hail.autoscrollbacklayoutdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListViewDemoActivity.start(MainActivity.this);
+            }
+        });
+        findViewById(R.id.btn_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                GridViewDemoActivity.start(MainActivity.this);
+            }
+        });
+        findViewById(R.id.btn_3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RecyclerViewDemoActivity.start(MainActivity.this);
+            }
+        });
+    }
+}
